36cfab2822e9178b22cba21d43cc193e21767721
sowenowsky <113459363+sowenowsky@users.noreply.github.com>
2022-12-05 20:28:19 +0500

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,20 +1,6 @@
 from random import randint
 import logging
-
-logger_obj = logging.getLogger(__name__)
-
-w_handler = logging.StreamHandler()
-e_handler = logging.FileHandler('8LabaLog.log')
-w_handler.setLevel(logging.WARNING)
-e_handler.setLevel(logging.ERROR)
-
-c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-w_handler.setFormatter(c_format)
-e_handler.setFormatter(f_format)
-
-logger_obj.addHandler(w_handler)
-logger_obj.addHandler(e_handler)
+logging.basicConfig(filename="Логирование_8_задание.log", level=logging.INFO)
 
 
 def Barrel_Check():  # обработчик ошибки ввода кол-ва бочонков
@@ -37,16 +23,19 @@ def i_Check():  # обработчик ошибки ввода числа i
 
 All_Num = [] #массив со всеми "выпавшими" числами
 Barrel = Barrel_Check()
+logging.info('Пользователь выбрал данное кол-во бочек: ' + str(Barrel))
 k = 0
 for k in range (1, Barrel+1): #основной цикл
     print("1 - Достать случайное число\n2 - Закончить ")
     i = i_Check() #пользователь решает что сделать
+    logging.info('Между 1 (достать случайное число) и 2 (закончить) пользователь выбрал ' + str(i))
     if i == 1:
         num = randint(1, Barrel) #узнаем число
         if num in All_Num: #проверка выпадение этого числа ранее
             while num in All_Num: #цикл, чтобы выбрать другое число
                 num = randint(1, Barrel)
         print ("В этот раз выпало число: ", num, "\n")
+        logging.info('Пользователю выпало число ' + str(num))
         All_Num.append(num) #записываем число в массив
     if k == Barrel:
         print("Вы достали эти числа: ", All_Num)
